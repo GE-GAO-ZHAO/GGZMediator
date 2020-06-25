4ef481bf859f780dbb29a7412ea7f05d57639669
xes <gegaozhao@100tal.com>
2020-06-25 16:46:28 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-TALMediator_Tests/Pods-TALMediator_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TALMediator_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TALMediator_Tests
+@end
---
+++ Example/Pods/Target Support Files/TALMediator/TALMediator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TALMediator : NSObject
+@end
+@implementation PodsDummy_TALMediator
+@end
